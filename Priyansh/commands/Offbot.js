59a22e96ce9b6665b4566be7ef8b480c360f7f8f
gazizullah12 <gazizullah12@gmail.com>
2025-06-13 01:19:34 +0500

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -9,7 +9,7 @@ module.exports.config = {
         };
 module.exports.run = ({event, api}) =>{
     const permission = ["100077031182469", 
-	"100077031182469"];
+	"61553527333824"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Prince Dilshad", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
