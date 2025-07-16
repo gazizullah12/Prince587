7425882fe3fb3400be05638cff959b5c7efdb83f
gazizullah12 <gazizullah12@gmail.com>
2025-07-16 14:16:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ try {
     logger(packageInfo.description, "[ DESCRIPTION ]");
     
     // Try to check for updates, but don't stop the bot if it fails
-    axios.get("https://raw.githubusercontent.com/codedbypriyansh/Priyansh-Bot/main/package.json")
+    axios.get("https://raw.githubusercontent.com/codedbyprincedilshad/Prince-Bot/main/package.json")
         .then((res) => {
             // Only log if successful, don't stop the bot if there's an error
             if (res.data && res.data.version) {
